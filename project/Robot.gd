0917456ce76eeefc9fdd589b87a2579f6a8ba1d2
the-alex-g <alex.gestwicki@gmail.com>
2022-08-15 13:31:42 -0400

    Move falling logic to robot.gd

--- project/Robot.gd
+++ project/Robot.gd
@@ -1,7 +1,7 @@
 class_name Robot
 extends KinematicBody2D
 
-const GRAVITY := 15
+const GRAVITY := 20
 
 export var power_crystals := 1
 export var health := 6
@@ -13,3 +13,16 @@ var _drones := System.new(false, 0)
 var _horizontal_speed := 200
 var _max_fall_speed := 600
 var _current_vertical_speed := 0
+
+
+func _physics_process(_delta:float)->void:
+	# handle falling
+	if is_on_floor():
+		# reset gravity if on floor
+		if _current_vertical_speed != 0:
+			_current_vertical_speed = 0
+	else: # apply gravity
+		if is_on_ceiling() and _current_vertical_speed < 0: # if you hit a ceiling, stop going up
+			_current_vertical_speed = 0
+		if _current_vertical_speed < _max_fall_speed:
+			_current_vertical_speed += GRAVITY
